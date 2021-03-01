c8c46fa5c641631d1453ae9a79fb4f804c0d32a8
mosesfawole <55624166+mosesfawole@users.noreply.github.com>
2021-03-01 03:59:07 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.Port || 5000;
+
+app.use("/", express.static(path.join(__dirname, "/client/build")));
 
 app.use(cors());
 app.use(express.json());
@@ -34,6 +37,9 @@ const usersRouter = require("./routes/users");
 app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+});
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
